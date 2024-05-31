04288fd7640ef8841a53f416a2891a00826ffa2a
Meng Lin <menglinmaker@gmail.com>
2024-05-31 16:12:39 +1000

    DX: Init schema for todo test

---
+++ cli/src/drizzle/0000_blushing_hemingway.sql
@@ -0,0 +1,55 @@
+CREATE TABLE IF NOT EXISTS "Todo" (
+	"todo_id" bigserial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"description" text,
+	"start_time" timestamp DEFAULT NULL,
+	"deadline_time" timestamp DEFAULT NULL,
+	"done" boolean DEFAULT false NOT NULL,
+	"created_time" timestamp DEFAULT now() NOT NULL,
+	"updated_time" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "Todo_Tag" (
+	"todo_tag_id" bigserial PRIMARY KEY NOT NULL,
+	"todo_id" bigserial NOT NULL,
+	"title" text NOT NULL,
+	"description" text,
+	"created_time" timestamp DEFAULT now() NOT NULL,
+	"updated_time" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "Users" (
+	"user_id" bigserial PRIMARY KEY NOT NULL,
+	"first_name" text NOT NULL,
+	"last_name" text NOT NULL,
+	"email" text NOT NULL,
+	"created_time" timestamp DEFAULT now() NOT NULL,
+	"updated_time" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "Users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "Users_Todo" (
+	"todo_id" bigserial NOT NULL,
+	"user_id" bigserial NOT NULL,
+	"done" boolean DEFAULT false NOT NULL,
+	"created_time" timestamp DEFAULT now() NOT NULL,
+	"updated_time" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "Todo_Tag" ADD CONSTRAINT "Todo_Tag_todo_id_Todo_todo_id_fk" FOREIGN KEY ("todo_id") REFERENCES "public"."Todo"("todo_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "Users_Todo" ADD CONSTRAINT "Users_Todo_user_id_Users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."Users"("user_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "Users_Todo" ADD CONSTRAINT "Users_Todo_todo_id_Todo_todo_id_fk" FOREIGN KEY ("todo_id") REFERENCES "public"."Todo"("todo_id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
---
+++ cli/src/schema.ts
@@ -0,0 +1,48 @@
+import { sql } from 'drizzle-orm'
+import {
+  text,
+  timestamp,
+  pgTable,
+  bigserial,
+  boolean
+} from 'drizzle-orm/pg-core'
+
+const _bigserial = (key) => bigserial(key, { mode: 'number' })
+
+export const Users_Table = pgTable('Users', {
+  user_id: _bigserial('user_id').primaryKey(),
+  first_name: text('first_name').notNull(),
+  last_name: text('last_name').notNull(),
+  email: text('email').notNull().unique(),
+  created_time: timestamp('created_time').defaultNow().notNull(),
+  updated_time: timestamp('updated_time').defaultNow().notNull()
+})
+
+export const Todo_Table = pgTable('Todo', {
+  todo_id: _bigserial('todo_id').primaryKey(),
+  title: text('title').notNull(),
+  description: text('description'),
+  start_time: timestamp('start_time').default(sql`NULL`),
+  deadline_time: timestamp('deadline_time').default(sql`NULL`),
+  done: boolean('done').notNull().default(false),
+  created_time: timestamp('created_time').defaultNow().notNull(),
+  updated_time: timestamp('updated_time').defaultNow().notNull()
+})
+
+export const Users_Todo_Table = pgTable('Users_Todo', {
+  users_todo_id: _bigserial('todo_id').primaryKey(),
+  user_id: _bigserial('user_id').references(() => Users_Table.user_id),
+  todo_id: _bigserial('todo_id').references(() => Todo_Table.todo_id),
+  can_edit: boolean('done').notNull().default(false),
+  created_time: timestamp('created_time').defaultNow().notNull(),
+  updated_time: timestamp('updated_time').defaultNow().notNull()
+})
+
+export const Todo_Tag_Table = pgTable('Todo_Tag', {
+  todo_tag_id: _bigserial('todo_tag_id').primaryKey(),
+  todo_id: _bigserial('todo_id').references(() => Todo_Table.todo_id),
+  title: text('title').notNull(),
+  description: text('description'),
+  created_time: timestamp('created_time').defaultNow().notNull(),
+  updated_time: timestamp('updated_time').defaultNow().notNull()
+})
