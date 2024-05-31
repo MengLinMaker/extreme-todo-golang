be13ee78a121bdb62100b73aa21ff0868dee74c4
Meng Lin <menglinmaker@gmail.com>
2024-05-31 23:04:10 +1000

    DX: Move all database related files under one folder

--- cli/drizzle.config.ts
+++ cli/drizzle.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'drizzle-kit'
+import 'dotenv'
+require('dotenv').config()
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/schema.ts',
-  out: './src/drizzle'
+  schema: './src/database/schema.ts',
+  out: './src/database/drizzle',
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!
+  }
 })
