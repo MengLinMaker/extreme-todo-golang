5df38d2781ce8b518c8d19b4fa764500a8e5c230
Meng Lin <menglinmaker@gmail.com>
2024-06-03 09:57:01 +1000

    DX: Add postgres stats

--- cli/src/database/db.ts
+++ cli/src/database/db.ts
@@ -2,8 +2,13 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 require('dotenv').config()
 import { exec } from 'child_process'
+import { sql } from 'drizzle-orm'
 
 exec('pnpm run migrate')
 
 const queryClient = postgres(process.env.POSTGRES_URL!)
 export const db = drizzle(queryClient)
+
+  ; (async () => {
+    await db.execute(sql`CREATE EXTENSION IF NOT EXISTS pg_stat_statements`)
+  })()
