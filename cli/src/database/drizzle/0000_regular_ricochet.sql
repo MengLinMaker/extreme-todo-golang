4379ce474b38be479185388eeb95c2b672195bf9
Meng Lin <menglinmaker@gmail.com>
2024-06-02 16:32:47 +1000

    DX: User lower case tables to avoid case sensitive query

--- cli/src/database/drizzle/0000_regular_ricochet.sql
+++ cli/src/database/drizzle/0000_regular_ricochet.sql
@@ -1,4 +1,13 @@
-CREATE TABLE IF NOT EXISTS "Todo" (
+CREATE TABLE IF NOT EXISTS "todo_tags" (
+	"todo_tag_id" bigserial PRIMARY KEY NOT NULL,
+	"todo_id" bigserial NOT NULL,
+	"title" text NOT NULL,
+	"description" text,
+	"created_time" timestamp DEFAULT now() NOT NULL,
+	"updated_time" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "todos" (
 	"todo_id" bigserial PRIMARY KEY NOT NULL,
 	"title" text NOT NULL,
 	"description" text,
@@ -9,27 +18,18 @@ CREATE TABLE IF NOT EXISTS "Todo" (
 	"updated_time" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "Todo_Tag" (
-	"todo_tag_id" bigserial PRIMARY KEY NOT NULL,
-	"todo_id" bigserial NOT NULL,
-	"title" text NOT NULL,
-	"description" text,
-	"created_time" timestamp DEFAULT now() NOT NULL,
-	"updated_time" timestamp DEFAULT now() NOT NULL
-);
---> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "Users" (
+CREATE TABLE IF NOT EXISTS "users" (
 	"user_id" bigserial PRIMARY KEY NOT NULL,
 	"first_name" text NOT NULL,
 	"last_name" text NOT NULL,
 	"email" text NOT NULL,
 	"created_time" timestamp DEFAULT now() NOT NULL,
 	"updated_time" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "Users_email_unique" UNIQUE("email")
+	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "Users_Todo" (
-	"users_todo_id" bigserial PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS "users_todos" (
+	"user_todo_id" bigserial PRIMARY KEY NOT NULL,
 	"user_id" bigserial NOT NULL,
 	"todo_id" bigserial NOT NULL,
 	"done" boolean DEFAULT false NOT NULL,
@@ -38,19 +38,19 @@ CREATE TABLE IF NOT EXISTS "Users_Todo" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "Todo_Tag" ADD CONSTRAINT "Todo_Tag_todo_id_Todo_todo_id_fk" FOREIGN KEY ("todo_id") REFERENCES "public"."Todo"("todo_id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "todo_tags" ADD CONSTRAINT "todo_tags_todo_id_todos_todo_id_fk" FOREIGN KEY ("todo_id") REFERENCES "public"."todos"("todo_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "Users_Todo" ADD CONSTRAINT "Users_Todo_user_id_Users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."Users"("user_id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "users_todos" ADD CONSTRAINT "users_todos_user_id_users_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("user_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "Users_Todo" ADD CONSTRAINT "Users_Todo_todo_id_Todo_todo_id_fk" FOREIGN KEY ("todo_id") REFERENCES "public"."Todo"("todo_id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "users_todos" ADD CONSTRAINT "users_todos_todo_id_todos_todo_id_fk" FOREIGN KEY ("todo_id") REFERENCES "public"."todos"("todo_id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- cli/src/database/schema.ts
+++ cli/src/database/schema.ts
@@ -9,7 +9,7 @@ import {
 
 const _bigserial = (key: string) => bigserial(key, { mode: 'number' })
 
-export const Users_Table = pgTable('Users', {
+export const Users_Table = pgTable('users', {
   user_id: _bigserial('user_id').primaryKey(),
   first_name: text('first_name').notNull(),
   last_name: text('last_name').notNull(),
@@ -18,7 +18,7 @@ export const Users_Table = pgTable('Users', {
   updated_time: timestamp('updated_time').defaultNow().notNull()
 })
 
-export const Todo_Table = pgTable('Todo', {
+export const Todos_Table = pgTable('todos', {
   todo_id: _bigserial('todo_id').primaryKey(),
   title: text('title').notNull(),
   description: text('description'),
@@ -29,18 +29,18 @@ export const Todo_Table = pgTable('Todo', {
   updated_time: timestamp('updated_time').defaultNow().notNull()
 })
 
-export const Users_Todo_Table = pgTable('Users_Todo', {
-  users_todo_id: _bigserial('users_todo_id').primaryKey(),
+export const Users_Todos_Table = pgTable('users_todos', {
+  user_todo_id: _bigserial('user_todo_id').primaryKey(),
   user_id: _bigserial('user_id').references(() => Users_Table.user_id),
-  todo_id: _bigserial('todo_id').references(() => Todo_Table.todo_id),
+  todo_id: _bigserial('todo_id').references(() => Todos_Table.todo_id),
   can_edit: boolean('done').notNull().default(false),
   created_time: timestamp('created_time').defaultNow().notNull(),
   updated_time: timestamp('updated_time').defaultNow().notNull()
 })
 
-export const Todo_Tag_Table = pgTable('Todo_Tag', {
+export const Todo_Tags_Table = pgTable('todo_tags', {
   todo_tag_id: _bigserial('todo_tag_id').primaryKey(),
-  todo_id: _bigserial('todo_id').references(() => Todo_Table.todo_id),
+  todo_id: _bigserial('todo_id').references(() => Todos_Table.todo_id),
   title: text('title').notNull(),
   description: text('description'),
   created_time: timestamp('created_time').defaultNow().notNull(),
